e3426e908178c1115565c66244adc893baf53939
Luix <luiz.delgado@eniac.edu.br>
2024-03-06 17:03:45 -0300

    Aula 06/03

---
+++ envio_informacoes.py
@@ -0,0 +1,39 @@
+#Importar as bibliteocas
+import paho.mqtt.client as mqtt #Biblioteca do protocolo mqtt
+import time
+
+#Credenciais do broker
+BROKER = "test.mosquitto.org"
+PORT = 1883
+
+#Criar função de conexão
+def on_connect(client, userdata, flgs, rc):
+    if rc==0:
+        print("Conexão bem sucedida!")
+    else:
+        print("Não foi possivel se conectar ao servidor!")
+
+#Criar o cliente
+cliente = mqtt.Client() #mqtt.CallbackAPIVersion.VERSION1
+
+#Conectar no broker
+cliente.connect(BROKER, port=PORT)
+
+#Verificar a conexão
+cliente.on_connect = on_connect
+
+
+#Publicar a mesagem
+cliente.publish("aula_iot_3ano", "LIGA_LED_VERMELHO")
+time.sleep(1)
+cliente.publish("aula_iot_3ano", "DESLIGA_LED_VERMELHO")
+time.sleep(1)
+cliente.publish("aula_iot_3ano", "LIGA_LED_VERMELHO")
+time.sleep(1)
+cliente.publish("aula_iot_3ano", "DESLIGA_LED_VERMELHO")
+
+
+
+
+#Desconectar do server
+cliente.disconnect()
